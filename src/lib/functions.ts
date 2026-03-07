6af8c6b8526563322c8d662ba94051a09814edd2
mrlachnowicz-hash <mrlachnowicz@gmail.com>
2026-03-07 22:34:24 +0100

    Delete src directory

--- src/lib/functions.ts
+++
@@ -1,8 +0,0 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
-import { app } from "./firebase";
-
-export const functions = getFunctions(app, "europe-west1");
-
-export function callable<TReq = any, TRes = any>(name: string) {
-  return httpsCallable<TReq, TRes>(functions, name);
-}
